7eff190fae416a7908c45e5c2a9a66a2bbc30298
Afam <afam262@gmail.com>
2021-01-19 20:38:56 -0600

    adding update route and removing delete route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,53 +6,40 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 
-// stuck here .........
+
+// All routes set up 
 router.get("/", function(req, res) {
-  res.redirect("/burgers")
-})
-// Create all our routes and set up logic within those routes where required.
-router.get("/burgers", function(req, res) {
-    burger.all(function(data) {
-    var hbsObject = {
-        burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+    burger.all(function(data) {      
+    res.render("index", {burgers: data});
   });
 });
 
-router.post("/burgers/create", function(req, res) {
-    burger.create(["burger_name", "devoured"], [req.body.name, req.body.vegan  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+router.post("/api/burgers", function(req, res) {
+  burger.create(req.body.name, function(result) {
+    res.json({id: result.insertID});
   });
+
+  //   burger.create(["burger_name", "devoured"], [req.body.name, req.body.vegan  ], function(result) {
+  //   // Send back the ID of the new quote
+  //   res.json({ id: result.insertId });
+  // });
 });
 
-router.put("/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  burger.update({vegan: req.body.vegan}, condition, function(result) {
-    if (result.changedRows === 0) {
+  burger.update(1, req.params.id, function(result) {
+    if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
     }
-  });w
+  });
 });
 
-router.delete("api/burgers/:id", async function(req, res) {
-    let columName = "id";
-    let columValue = req.params.id;
 
-    const result = await burger.modelDeleteAsyncExample(columName, columValue);
-
-    if (result.affectedRows === 0) {
-        return res.status(404).end();
-    }
-    res.status(200).end();
-});
 
 module.exports = router;
