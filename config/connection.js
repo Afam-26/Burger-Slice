ddfb075dce282e1faeb7919aef45b6415e1539a5
Afam <afam262@gmail.com>
2021-01-21 01:29:40 -0600

    recreating heroku

--- config/connection.js
+++ config/connection.js
@@ -7,12 +7,12 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",  
         port: 3306,     
-        user: "process.env.DB_USERNAME",
-        password: "process.env.DB_PASSWORD",
-        database: "process.env.DB_DATABASE"
-    })
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
+    });
         
-};
+}
 
 
 
